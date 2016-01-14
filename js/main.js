7c140929d5517b838b3b9858643ff7c08fdf4082
Mat Gilbert <matgilbert@gmail.com>
2016-01-13 23:37:17 -0500

    Added sliders, update img on slider change

--- js/main.js
+++ js/main.js
@@ -1,16 +1,45 @@
 'use strict';
 
+
+$(function() { 
+
+  $("#imgBrightnessValue").slider({
+    tooltip: 'always'
+  });
+
+  $("#imgContrastValue").slider({
+    tooltip: 'always'
+  });  
+
+  $("#imgExposure").slider({
+    tooltip: 'always'
+  });  
+
+  $("#imgClip").slider({
+    tooltip: 'always'
+  });
+
+  $('#imageSettings').on('change','.imageSettingSlider',function(){
+
+      console.log('YEAH!');
+      processImg();
+
+  });  
+
+});
+
 function previewFile() {
   var uploadContainer = document.querySelector('#uploadContainer');
+  var imageSettings = document.querySelector('#imageSettings');
   var greyscale = document.querySelector('#imgGreyscale');
   var preview = document.querySelector('#imgSubmit');
-  var greyscale = document.querySelector('#imgGreyscale');
   var file    = document.querySelector('#imgToProcess').files[0];
   var reader  = new FileReader();
 
   reader.onloadend = function () {
     
   	$(uploadContainer).slideUp();
+    $(imageSettings).slideDown();
     preview.src = reader.result;
     convertGreyscale();
 
@@ -32,17 +61,28 @@ function previewFile() {
 function processImg(){
   
   var preview = document.querySelector('#imgSubmit');
-  var brightness = document.querySelector('#imgBrightness');
-  var contrast = document.querySelector('#imgContrast');  
+
+  var brightness = document.querySelector('#imgBrightnessValue');
+  var contrast = document.querySelector('#imgContrastValue');
+
+  var targetCanvas = document.querySelector('#imgGreyscale');
+
+  //targetCanvas.clearRect(0, 0, targetCanvas.width, targetCanvas.height);  
 
   Caman('#imgGreyscale',preview.src, function(){
+
+    this.render();
 
     this.resize({width: 260});
     
     if(contrast.value != 0) this.contrast(contrast.value);
     if(brightness.value != 0) this.brightness(brightness.value);
  
+    console.log('contrast: ' + contrast.value, 'brightness: ' + brightness.value)
+
     this.render();
+    convertSVG();
+
 
   });
 
@@ -71,10 +111,29 @@ function convertGreyscale(){
 
 }
 
+function svgShapeToTrace(){
+
+  var svgContainer = $('#svgContainer').children();
+  var traceContainer = document.querySelector('#traceContainer');
+  
+  //console.log(svgContainer[0]);
+  //svgContainer[0];
+
+  traceContainer.innerHTML = svgContainer[0];
+
+  var traceSVG = $('#traceContainer').children();
+
+  $(traceSVG[0]).prop('fill','none');
+  $(traceSVG[0]).prop('stroke','#000000');
+
+}
+
 function convertSVG(){
 
 	var greyscale = document.querySelector('#imgGreyscale');
 	var svgContainer = document.querySelector('#svgContainer');
+
+  svgContainer.innerHTML = '';
 
 	console.log(greyscale.toDataURL('image/png'));
 
@@ -82,6 +141,9 @@ function convertSVG(){
 	//Potrace.loadImageFromFile(greyscale.toDataURL('image/png'));
 	Potrace.process(function(){
 		svgContainer.innerHTML = Potrace.getSVG(1);
+    //svgShapeToTrace();
 	});
 
 }
+
+
