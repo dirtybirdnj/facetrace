8dd5ca5fa60b47b0774e0e7314b65a3c6573b7c4
Mat Gilbert <matgilbert@gmail.com>
2016-01-27 10:53:09 -0500

    Added function/button for SVG download

--- js/refactor.js
+++ js/refactor.js
@@ -251,6 +251,35 @@ var FaceTrace = {
 		//Re-draw workspace with the new setting
 		this.processImage();
 
+	},
+
+	downloadSVG: function(event){
+
+			var svg = document.getElementById("svg");
+
+			//get svg source.
+			var serializer = new XMLSerializer();
+			var source = serializer.serializeToString(svg);
+
+			//add name spaces.
+			if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+			    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+			}
+			if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
+			    source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+			}
+
+			//add xml declaration
+			source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+			//convert svg source to URI data scheme.
+			var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+
+			//set url value to a element's href attribute.
+			document.getElementById("link").href = url;
+
+
+
 	}
 
 };
